b0a014128b494611f9a786ef7a1be47e47457bc4
Tobi-Jakka <42676160+Tobi-Jakka@users.noreply.github.com>
2021-11-27 20:11:18 +0100

    Cleanup (1/*)

--- src/main/java/tobi/spawnmob/CommandHandler.java
+++ src/main/java/tobi/spawnmob/CommandHandler.java
@@ -8,32 +8,10 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
-import static java.lang.Integer.parseInt;
 import static org.bukkit.Bukkit.getServer;
 
 public class CommandHandler implements CommandExecutor {
 
-    public static boolean isNumeric(String string) {
-        int intValue;
-
-        System.out.println(String.format("Parsing string: \"%s\"", string));
-
-        if(string == null || string.equals("")) {
-            System.out.println("String cannot be parsed, it is null or empty.");
-            return false;
-        }
-
-        try {
-            intValue = parseInt(string);
-            return true;
-        } catch (NumberFormatException e) {
-            System.out.println("Input String cannot be parsed to Integer.");
-        }
-        return false;
-    }
-
-
-    //public SpawnMob() {}
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -56,7 +34,7 @@ public class CommandHandler implements CommandExecutor {
                 quantityMobs = Integer.parseInt(args[1]);
 
                 if(Integer.parseInt(args[1]) > 8){
-                    quantityMobs = 8; //if the the quantityOfMonsters is > 8 then just spawn 8. (Change to config later)
+                    quantityMobs = 8; //if the quantityOfMonsters is > 8 then just spawn 8. (Change to config later)
                 }
             }
 
@@ -82,57 +60,5 @@ public class CommandHandler implements CommandExecutor {
 
         return true;
     }
-
-
-
-    /* public boolean onCommand1(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player)
-        {
-            Player p = (Player) sender;
-            Location loc = p.getLocation();
-
-            System.out.println("Args length:" + args.length);
-            if(args.length == 0) {
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Syntax: /spawnmob <entity>"));
-                //return false;
-            }
-            else {
-                switch(args[0].toUpperCase()) {
-                    case "ZOMBIE":
-                        if (args.length == 1) {
-                            loc.getWorld().spawnEntity(loc, EntityType.ZOMBIE);
-                            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cSpawned &b1 &cZombie."));
-                            
-                        }
-                        else if (args.length == 2) {
-                            if (!isNumeric(args[1])) return false;
-                            for(int i = 0; i  < parseInt(args[1]); i++) {
-                                loc.getWorld().spawnEntity(loc,EntityType.ZOMBIE);
-                            }
-                            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cSpawned &b" + args[1] + " &cZombie."));
-                        }
-                        else if (args.length == 3) {
-                            Player t = getServer().getPlayer(args[2]);
-                            Location tloc = t.getLocation();
-
-                            for (int i = 0; i < parseInt(args[1]); i++) {
-                                tloc.getWorld().spawnEntity(tloc, EntityType.ZOMBIE);
-                            }
-                            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cSpawned &b" + args[1] + " &cZombie on &b" + args[2] + "&c."));
-                        }
-                        break;
-
-                    case "SKELETON":
-                        for(int i = 0; i  < parseInt(args[1]); i++) {
-                            loc.getWorld().spawnEntity(loc,EntityType.SKELETON);
-                            System.out.println("Skeleton Spawned");
-                        }
-                        break;
-                    }
-                }
-                return true;
-            }
-        return false;
-        } */
-    }
+}
 
