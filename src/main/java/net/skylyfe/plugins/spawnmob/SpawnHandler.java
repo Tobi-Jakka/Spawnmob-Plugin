02d6f20d0926edcc695dbc977bb49d1da520b6f1
Tobi-Jakka <42676160+Tobi-Jakka@users.noreply.github.com>
2021-11-30 02:03:37 +0100

    Spawnmob Update 4

--- src/main/java/net/skylyfe/plugins/spawnmob/SpawnHandler.java
+++ src/main/java/net/skylyfe/plugins/spawnmob/SpawnHandler.java
@@ -9,29 +9,66 @@ import static org.bukkit.Bukkit.getServer;
 
 public class SpawnHandler {
 
+    public static boolean typeTester(String input) {
+        try {
+        EntityType EntityMonster = EntityType.valueOf(input.toUpperCase());
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean amountTester(String input) {
+        try {
+            int quantity = Integer.parseInt(input);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean locationTester(String input) {
+        try {
+            Location targetLocation = getServer().getPlayer(input).getLocation();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
     public static boolean spawnMonster(Player player, String[] args){
         if (args.length < 1) return true;
 
         String monster = args[0];
-        EntityType EntityMonster = EntityType.valueOf(monster.toUpperCase());
-
-        Location targetLocation = player.getLocation();
         int quantity = 1;
 
-        if (args.length >= 3) {
-            targetLocation = getServer().getPlayer(args[2]).getLocation();
+        if (!typeTester(monster)) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',  "&cInvalid type"));
+            return true;
         }
+        EntityType EntityMonster = EntityType.valueOf(monster.toUpperCase());
+
 
         if (args.length >= 2) {
-            try {
-            quantity = Integer.parseInt(args[1]);
-            } catch (Exception e) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',  "&cInvalid amount"));
+            if (!amountTester(args[1])) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cInvalid amount"));
                 return true;
             }
+            quantity = Integer.parseInt(args[1]);
+
             if (quantity < 1 || quantity > 4) {
                 quantity = 1;
             }
+        }
+
+        Location targetLocation = player.getLocation();
+
+        if (args.length >= 3) {
+            if (!locationTester(args[2])) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cInvalid player"));
+                return true;
+            }
+            targetLocation = getServer().getPlayer(args[2]).getLocation();
         }
 
         for (int i = 0; i < quantity; i++) {
